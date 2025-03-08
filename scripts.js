71949b72b252f6548dfef169b0d592848cfed6b9
danieldema <danieldema42@gmail.com>
2025-03-07 20:13:56 -0500

    added transitions

---
+++ scripts.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const transition_el = document.querySelector('.transition')
+    const anchors = document.querySelectorAll('.nav-links a');
+
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 350);
+
+    for (let i = 0; i < anchors.length; i++) {
+            const anchor = anchors[i];
+
+            anchor.addEventListener('click', e => {
+                e.preventDefault();
+                let target = e.target.href;
+
+                console.log(target);
+
+                transition_el.classList.add('is-active');
+
+                setTimeout(() => {
+                    window.location.href = target;
+                }, 350);
+            });
+        }
+    }
